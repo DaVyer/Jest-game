a64a2fc85ae13f3cf575eb622780006a152b7c24
DaVyer <rodrigues.gwendal@gmail.com>
2025-12-16 13:01:57 +0100

    doc(sauvegarde): Ajout de la documentation

--- src/classe/Load.java
+++ src/classe/Load.java
@@ -4,7 +4,27 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 
+/**
+ * Classe utilitaire pour charger une partie sauvegardée.
+ * 
+ * <p>Permet de désérialiser une partie depuis un fichier
+ * en utilisant les objets DTO.</p>
+ * 
+ * @author Gwendal Rodrigues
+ * @version %I%, %G%
+ */
 public class Load {
+    /**
+     * Charge une partie depuis un fichier.
+     * 
+     * <p>Désérialise le PartieDTO depuis le fichier spécifié
+     * et le convertit en objet Partie.</p>
+     * 
+     * @param fichier le chemin du fichier à charger
+     * @return la partie chargée
+     * @throws IOException si une erreur d'entrée/sortie survient
+     * @throws ClassNotFoundException si la classe PartieDTO n'est pas trouvée
+     */
     public static Partie charger(String fichier)
             throws IOException, ClassNotFoundException {
 
--- src/classe/Save.java
+++ src/classe/Save.java
@@ -4,7 +4,26 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 
+/**
+ * Classe utilitaire pour sauvegarder une partie en cours.
+ * 
+ * <p>Permet de sérialiser une partie dans un fichier
+ * en utilisant les objets DTO.</p>
+ * 
+ * @author Gwendal Rodrigues
+ * @version %I%, %G%
+ */
 public class Save {
+    /**
+     * Sauvegarde une partie dans un fichier.
+     * 
+     * <p>Convertit la partie en PartieDTO puis la sérialise
+     * dans le fichier spécifié.</p>
+     * 
+     * @param partie la partie à sauvegarder
+     * @param fichier le chemin du fichier de sauvegarde
+     * @throws IOException si une erreur d'entrée/sortie survient
+     */
     public static void sauvegarder(Partie partie, String fichier) throws IOException {
 
         PartieDTO dto = DTOMapper.partieToDTO(partie);
