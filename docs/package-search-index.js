12837d5ea9349ad4674296c3c10edecb14571f54
DaVyer <rodrigues.gwendal@gmail.com>
2026-01-15 18:04:22 +0100

    run de la commande javadoc

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"controller"},{"l":"main"},{"l":"model"},{"l":"service"},{"l":"view"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controller"},{"l":"main"},{"l":"model"},{"l":"service"},{"l":"view"}];updateSearchResults();
